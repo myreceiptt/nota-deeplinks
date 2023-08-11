f95f91a86c50c0b3dacda926523c28595d03585c
Prof. NOTA <141787406+myreceiptt@users.noreply.github.com>
2023-08-11 22:42:43 +0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+export default class extends Component {
+  render () {
+    return (
+      <><div><h4>DevOps: Initialization Page!</h4></div><div>
+            <p>Please visit this link: <a href="https://deeplinks.straight-line.org/" target="_blank" rel="noreferrer">Prof. NOTA's Deep Links</a></p>
+            <p><i>Please note that this link merely provide list of <a href="https://deeplinks.straight-line.org/" target="_blank" rel="noreferrer">Prof. NOTA's Deep Links</a> and does not facilitate the access to or use of the Deep Links.</i></p>
+        </div></>
+    )
+  }
+}
